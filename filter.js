fe53237d5bb2087d5a4a44a28db635856350e2aa
psxvalues <125580181+psxvalues@users.noreply.github.com>
2023-02-19 11:48:24 +0000

    Update filter.js

--- filter.js
+++ filter.js
@@ -4,13 +4,28 @@ function filterFunction() {
   filter = input.value.toUpperCase();
   ul = document.getElementById("myUL");
   li = ul.getElementsByTagName("li");
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
+
+  // Convert li elements to an array and sort them alphabetically
+  var listItems = Array.prototype.slice.call(li);
+  listItems.sort(function(a, b) {
+    var aText = a.textContent || a.innerText;
+    var bText = b.textContent || b.innerText;
+    return aText.localeCompare(bText);
+  });
+
+  // Append sorted list items to the ul element
+  for (i = 0; i < listItems.length; i++) {
+    ul.appendChild(listItems[i]);
+  }
+
+  // Filter the sorted list based on user input
+  for (i = 0; i < listItems.length; i++) {
+    a = listItems[i].getElementsByTagName("a")[0];
     txtValue = a.textContent || a.innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
+      listItems[i].style.display = "";
     } else {
-      li[i].style.display = "none";
+      listItems[i].style.display = "none";
     }
   }
 }
